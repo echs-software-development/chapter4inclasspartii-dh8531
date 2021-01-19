d4f0811ca9f86d48826320b06c5c8c52d6e1dbaa
DYLAN HANNIGAN <dh8531@students.necsd.net>
2021-01-19 23:38:38 +0000

    i did lab questions 1 2 and 3 all properly!!!!

--- Prob2.java
+++ Prob2.java
@@ -3,5 +3,10 @@ import java.util.Scanner;
 class Prob2 {
   public static void main(String[] args) {
     
+  Scanner scan = new Scanner(System.in);
+  System.out.println("Enter a string");
+  String word = scan.nextLine();
+  System.out.println(word.substring(0));
+
   }
 }
